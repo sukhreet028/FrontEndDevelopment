eda2f4230883676741c62d2c5cf96b71ecd237a9
Sukhvir2104 <sukhvir21@live.com>
2021-07-14 18:58:09 -0400

    Javascript improvements

--- java/java1.js
+++ java/java1.js
@@ -1,19 +1,19 @@
-/*function firstFunction(){
-document.write("<h1 style='text-align:center;color:red;'>My Heading1</h>");
-document.write("<p style='color:blue; font-size:20px;margin:0px 50px;'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere perspiciatis sed cumque consequatur sit ad. Iste porro voluptatibus dolorem? Culpa vitae incidunt asperiores sint hic maxime dolores quibusdam optio temporibus!</p>");
-document.write("&nbsp;<button style='color:white;background-color:black;height:30px;;width:80px' type='button' >click here</button>");
-document.write("<br><br><br><br>");
-document.write("<h1 style='text-align:center;color:black;'>My Heading2</h>");
-document.write("<p style='color:green; font-size:20px;margin:0px 50px;'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere perspiciatis sed cumque consequatur sit ad. Iste porro voluptatibus dolorem? Culpa vitae incidunt asperiores sint hic maxime dolores quibusdam </p>");
-document.write("&nbsp;<button style='color:black;background-color:red;height:30px;;width:80px' type='button' >click here</button>");
+function firstFunction(){
+document.getElementById('first-column').innerHTML=`<h1 style='text-align:center;color:red;'>My Heading1</h>
+<p style='color:blue; font-size:20px;margin:0px 50px;'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere perspiciatis sed cumque consequatur sit ad. Iste porro voluptatibus dolorem? Culpa vitae incidunt asperiores sint hic maxime dolores quibusdam optio temporibus!</p>
+&nbsp;<button style='color:white;background-color:black;height:30px;;width:80px' type='button' >click here</button>`;
+document.getElementById('second-column').innerHTML=`<h1 style='text-align:center;color:black;'>My Heading2</h>
+<p style='color:green; font-size:20px;margin:0px 50px;'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere perspiciatis sed cumque consequatur sit ad. Iste porro voluptatibus dolorem? Culpa vitae incidunt asperiores sint hic maxime dolores quibusdam </p>
+&nbsp;<button style='color:black;background-color:red;height:30px;;width:80px' type='button' >click here</button>`
 };
 
-firstFunction();*/
+firstFunction();
 
 
 function secondFunction() {
     let name1="singh";
     let name2="kaur";
+    console.log('name1 '+ name1+ ' name2 '+ name2);
     var fullName= (name1 + "&nbsp;"+ name2);
     document.write(fullName);
     document.getElementById(1).style.color = 'green';
