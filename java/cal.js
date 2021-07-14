16b240bdab739e4c48f12553774bd8e75cee546d
sukhreet028 <sukhreet028@gmail.com>
2021-07-14 18:17:27 -0300

    javaScript class1

---
+++ java/cal.js
@@ -0,0 +1,7 @@
+var num1= prompt("Enter your First Number");
+var num2= prompt("Enter your FSecond Number");
+
+num1=parseInt(num1);
+num2=parseInt(num2);
+var result=num1+num2;
+document.write("Total is: " + result);
